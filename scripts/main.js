f1e49fd004b4480b01530e4e81436a6ff33748c7
yefermenn <yefersoncamilo3@gmail.com>
2025-03-07 23:39:35 -0500

    dia de la mujer

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 var numero=0;
+let cancion = new Audio('./multimedia/tema.mp3');
 function aparecer(){
     var seccion = document.getElementById('felicidad');
+    cancion.play();
     var boton=document.getElementById('boton');
     seccion.classList.add('mostrar');
     boton.classList.add('ocultar');
@@ -8,11 +10,5 @@ function aparecer(){
 }
 function cambiarColor(){
     var cuerpo= document.getElementById('cuerpo');
-    var colores = ["red","blue","purple","green"];
-    cuerpo.style.backgroundColor=colores[numero];
-    numero=numero+1;
-    console.log(numero);
-    if(numero==3){
-        numero=0;
-    }
+    cuerpo.style.backgroundColor=`#F5D9FF`;
 }
